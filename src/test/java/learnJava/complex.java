24a453d5fbc31ab2d26d962229aee2b7228dc860
vijay <vijayitsme6@gmail.com>
2024-03-17 12:41:43 +0530

    second commit..

---
+++ src/test/java/learnJava/complex.java
@@ -0,0 +1,20 @@
+package learnJava;
+
+public class complex {
+
+	public static void main(String[] args) {
+		int row = 5;    //to print L pattern program
+		
+	    for( int i=0;i<=row;i++) {
+	    	
+	    	for(int j=2*(row-i);j>0;j--) {
+	    		System.out.print(" ");
+	    	}
+	    	for(int j=0;j<i;j++) {
+	    		System.out.print("* ");
+	    	}
+	    	System.out.println(" ");
+	    }
+	}
+}
+		
--- src/test/java/learningSelenium/lecx.java
+++ src/test/java/learningSelenium/lecx.java
@@ -1,3 +1,4 @@
+
 package learningSelenium;
 
 import java.io.File;
