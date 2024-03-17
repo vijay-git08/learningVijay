d7e61b93da9d85de7fe036bbe2554236ff2c5c5d
ajaymarimuthu <ajayindia8001@gmail.com>
2024-03-17 15:08:41 +0530

    added adition string

--- src/test/java/learnJava/dummycheck.java
+++ src/test/java/learnJava/dummycheck.java
@@ -4,7 +4,11 @@ public class dummycheck {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello");
+		
+		
+		String s="git ready";
+		System.out.println("Hello "+s);
+		
 		
 		
 	}
